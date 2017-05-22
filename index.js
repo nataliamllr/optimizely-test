c68d39010ef336bde6b099df10680fa72f27bc4e
Adrian Thomson <adrian.thomson@bbc.co.uk>
2017-05-22 22:48:25 +0100

    updated to use sandbox experiment, added a track event

--- index.js
+++ index.js
@@ -9,15 +9,29 @@ var optimizelyClient;
 
 var config = JSON.parse(fs.readFileSync(path.join(__dirname+'/config.json'), 'utf8'));
 
-optimizelyClient = optimizely.createInstance({datafile: config})
+var defaultLogger = require('optimizely-server-sdk/lib/plugins/logger');
 
+optimizelyClient = optimizely.createInstance({datafile: config, logger: defaultLogger.createLogger(
+{
+    logLevel: 1
+}) 
+});
 
 app.get('/', function (req, res) {
-var variationKey = optimizelyClient.activate("invalid_key", 'user123');
+var variationKey = optimizelyClient.activate('Segmentation', 'b58970b3f9', {});
+
+var eventTags = {
+	'link_location': 'hero-module',
+	'variant': variationKey,
+	'hp_module_name': 'promoTest'
+};
+
+optimizelyClient.track('TEST_EVENT', 'b58970b3f9', {}, eventTags);
+
 console.log("variationKey: ", variationKey);
 if (variationKey === 'default') {
   res.sendFile(path.join(__dirname+'/index.html'))
-} else if (variationKey === 'bucket_b') {
+} else if (variationKey === 'variant') {
   res.sendFile(path.join(__dirname+'/index_2.html'))
 } else {
   res.sendFile(path.join(__dirname+'/index.html'))
